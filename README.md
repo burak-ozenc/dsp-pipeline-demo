8cdff76be13a5382c4b0d85e66599d83d3484983
brk.ozenc <brk.ozenc@gmail.com>
2026-02-22 14:28:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+﻿# Audio Analytics Pipeline Demo
+Purpose is to create a pipeline over Prefect, extract basic DSP features from a sample music-speech-noise dataset, analyze, label and send it to pipeline.
---
+++ ingestion/discover.py
@@ -0,0 +1,17 @@
+﻿from prefect import flow, task 
+import pathlib
+from itertools import islice
+import hashlib
+
+@task
+def discover_files(path, limit=10):
+    res = []
+    for file_path in islice( pathlib.Path(path).rglob("*.wav"), limit):
+        res.append({
+            'file_path': str(file_path.relative_to(path)),
+            'file_size': str(file_path.stat().st_size),
+            'file_hash': hashlib.md5(file_path.read_bytes()).hexdigest(),
+            'file_name': file_path.name,    
+        })
+        
+    return res
